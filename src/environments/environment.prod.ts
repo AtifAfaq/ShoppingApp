6719dd791135ff5df688d371117e7387dfdda4d1
atif9322011 <atif9322011@gmail.com>
2019-05-10 03:08:06 -0400

    Managing dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2VWt-_bLWilXAdqZBCvMBIv1hoRHYkzk",
+    authDomain: "oshop-465a6.firebaseapp.com",
+    databaseURL: "https://oshop-465a6.firebaseio.com",
+    projectId: "oshop-465a6",
+    storageBucket: "oshop-465a6.appspot.com",
+    messagingSenderId: "1055126322381"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA2VWt-_bLWilXAdqZBCvMBIv1hoRHYkzk",
+    authDomain: "oshop-465a6.firebaseapp.com",
+    databaseURL: "https://oshop-465a6.firebaseio.com",
+    projectId: "oshop-465a6",
+    storageBucket: "oshop-465a6.appspot.com",
+    messagingSenderId: "1055126322381"
+  }
 };
 
 /*
